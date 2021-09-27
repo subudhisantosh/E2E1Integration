9a0e38238e50d524e2e66a3038b829d683454499
Santosh Subudhi <subudhisantosh01@gmail.com>
2021-09-28 00:43:23 +0530

    check-in my first maven project

---
+++ src/test/java/E2E/E2E1/TC_003.java
@@ -0,0 +1,21 @@
+package E2E.E2E1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_003 {
+	
+	@Test
+	public void testcase1()
+	{	
+		System.setProperty("webdriver.chrome.driver","./Drivers/chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		driver.findElement(By.id("login1")).sendKeys("subudhisantosh01");
+		driver.findElement(By.id("password")).sendKeys("J@isriRam01");
+		driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/div[2]/form/div[1]/div[2]/div[2]/div[2]/input[2]")).click();
+		driver.quit();
+	}
+
+}
